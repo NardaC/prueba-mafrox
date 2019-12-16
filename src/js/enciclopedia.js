5da2a4dc132e534111fa48bd9a9a865cc8694cf3
yenifer alexandra Galarza pezo <yeniferalexandra2000@hotmail.com>
2019-12-16 16:28:38 -0500

    cambios en enciclopedia de la carne

--- src/js/enciclopedia.js
+++ src/js/enciclopedia.js
@@ -1,6 +1,97 @@
 const pescuezoCircle = document.getElementById("pescuezo");
-const pescuezoText = document.getElementById('pescuezoText')
+const pescuezoText = document.getElementById('pescuezoText');
 
 pescuezoCircle.addEventListener('click', () => {
     pescuezoText.classList.toggle("invisible");
+});
+
+const segundoCorte = document.getElementById("segundoCorte");
+const segundoCorteText = document.getElementById('segundoCorteText');
+
+segundoCorte.addEventListener('click', () => {
+    segundoCorteText.classList.toggle("invisible");
+});
+
+const tercerCorte = document.getElementById("tercerCorte");
+const tercerCorteText = document.getElementById('tercerCorteText');
+
+tercerCorte.addEventListener('click', () => {
+    tercerCorteText.classList.toggle("invisible");
+});
+
+
+const cuartoCorte = document.getElementById("cuartoCorte");
+const cuartoCorteText = document.getElementById('cuartoCorteText');
+
+cuartoCorte.addEventListener('click', () => {
+    cuartoCorteText.classList.toggle("invisible");
+});
+
+
+const quintoCorte = document.getElementById("quintoCorte");
+const quintoCorteText = document.getElementById('quintoCorteText');
+
+quintoCorte.addEventListener('click', () => {
+    quintoCorteText.classList.toggle("invisible");
+});
+
+
+
+const sextoCorte = document.getElementById("sextoCorte");
+const sextoCorteText = document.getElementById('sextoCorteText')
+
+sextoCorte.addEventListener('click', () => {
+    sextoCorteText.classList.toggle("invisible");
+});
+
+
+const septimoCorte = document.getElementById("septimoCorte");
+const septimoCorteText = document.getElementById('septimoCorteText');
+
+septimoCorte.addEventListener('click', () => {
+    septimoCorteText.classList.toggle("invisible");
+});
+
+
+
+const octavoCorte = document.getElementById("octavoCorte");
+const octavoCorteText = document.getElementById('octavoCorteText');
+
+octavoCorte.addEventListener('click', () => {
+    octavoCorteText.classList.toggle("invisible");
+});
+
+const novenooCorte = document.getElementById("novenoCorte");
+const novenoCorteText = document.getElementById('novenoCorteText');
+
+novenoCorte.addEventListener('click', () => {
+    novenoCorteText.classList.toggle("invisible");
+});
+
+const decimoCorte = document.getElementById("decimoCorte");
+const decimoCorteText = document.getElementById('decimoCorteText');
+
+decimoCorte.addEventListener('click', () => {
+    decimoCorteText.classList.toggle("invisible");
+});
+
+
+
+
+
+
+const undecimoCorte = document.getElementById("undecimoCorte");
+const undecimoCorteText = document.getElementById('undecimoCorteText');
+
+undecimoCorte.addEventListener('click', () => {
+    undecimoCorteText.classList.toggle("invisible");
+});
+
+
+
+const duodecimoCorte = document.getElementById("duodecimoCorte");
+const duodecimoCorteText = document.getElementById('duodecimoCorteText');
+
+duodecimoCorte.addEventListener('click', () => {
+    duodecimoCorteText.classList.toggle("invisible");
 });
